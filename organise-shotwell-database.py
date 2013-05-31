52ec7b91a0249974f2810be36c575534c6892781
Filip Korzeniowski <filip.korzeniowski@jku.at>
2013-05-31 22:57:11 +0200

    added some options

--- organise-shotwell-database.py
+++ organise-shotwell-database.py
@@ -1,24 +1,42 @@
-import argparse
 import datetime as dt
 import sys
 import os
 import sqlite3 as db
 import shutil
+from argparse import ArgumentParser
 
-# TODO: make root dir configurable
-#       make destination dir optionally different from root dir
+# TODO:
 #       allow to choose between copy and move
-#       make DB file configurable
-#       make Date format configurable
 #       make directory names for events with name and without name configurable
 
-ROOT_DIR = '/home/felipe/Projekte/Programmieren/shotwell-events/photos'
-DB_FILE = '/home/felipe/datadir/data/photo.db'
+DB_FILE = os.path.join(os.getenv('HOME'), '.shotwell/data/photo.db')
 DATE_FORMAT = '%Y-%m-%d'
 
 
+def create_argparser():
+    parser = ArgumentParser(description='Reorganises shotwell\' photo directories')
+    parser.add_argument('destination_dir', help='Where to put the new photo directories')
+    parser.add_argument('--database', '-d', default=DB_FILE, dest='database_file',
+                        help='Path to shotwell\'s database file')
+    parser.add_argument('--date-format', default=DATE_FORMAT, dest='date_format',
+                        help='Date format, look up strftime for details.')
+
+    return parser
+
+
 def main():
-    conn = db.connect(DB_FILE)
+    parser = create_argparser()
+    args = parser.parse_args()
+
+    db_file = args.database_file
+    date_format = args.date_format
+    dest_dir = args.destination_dir
+
+    if not (os.path.exists(db_file) and os.path.isfile(db_file)):
+        print("Database file does not exist.")
+        return 1
+
+    conn = db.connect(db_file)
     conn.row_factory = db.Row
 
     event_select = 'SELECT id, name FROM EventTable'
@@ -29,20 +47,21 @@ def main():
     photo_update = 'UPDATE PhotoTable SET filename=? WHERE id=?'
 
     for event in conn.cursor().execute(event_select):
-        min_ts, max_ts = conn.cursor().execute(event_time, (event['id'],)).fetchone()
+        ts_cur = conn.cursor().execute(event_time, (event['id'],))
+        min_ts, max_ts = ts_cur.fetchone()
         min_date = dt.date.fromtimestamp(min_ts)
         max_date = dt.date.fromtimestamp(max_ts)
 
         if event['name']:
             # we have an event name
-            event_name = min_date.strftime(DATE_FORMAT) + ' - ' + event['name']
+            event_name = min_date.strftime(date_format) + ' - ' + event['name']
         else:
             # no event name
-            event_name = min_date.strftime(DATE_FORMAT)
+            event_name = min_date.strftime(date_format)
             if min_date != max_date:
-                event_name += ' - ' + max_date.strftime(DATE_FORMAT)
+                event_name += ' - ' + max_date.strftime(date_format)
 
-        new_folder_name = os.path.join(ROOT_DIR, event_name)
+        new_folder_name = os.path.join(dest_dir, event_name)
         if not os.path.exists(new_folder_name):
             os.mkdir(new_folder_name)
 
